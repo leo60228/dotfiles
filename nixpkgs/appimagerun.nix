8b3970bc7cd1f18ab6171bb0b8b6144ea8607f1b
leo60228 <leo@60228.dev>
2019-05-25 14:49:10 -0400

    Remove outdated overlays

--- nixpkgs/appimagerun.nix
+++
@@ -1,6 +0,0 @@
-self: super:
-{
-  appimage-run = super.appimage-run.override {extraPkgs = pkgs: with pkgs; [ (pkgs.writeShellScriptBin "pkexec" ''
-    ${sudo}/bin/sudo "$@"
-  '') ];};
-}
--- nixpkgs/mono.nix
+++
@@ -1,7 +0,0 @@
-self: super:
-
-#let nixpkgsMaster = import /home/leo60228/Programming/nixpkgs {}; in {
-#  mono = nixpkgsMaster.mono;
-#  msbuild = nixpkgsMaster.msbuild;
-#}
-{}
