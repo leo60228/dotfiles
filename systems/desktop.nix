520bd3d7df9a444b5be3ecad6a49b22a792e0955
leo60228 <leo@60228.dev>
2019-05-25 14:49:18 -0400

    Add some components to desktop

--- systems/desktop.nix
+++ systems/desktop.nix
@@ -5,7 +5,7 @@
 { config, pkgs, lib, ... }:
 
 with import ../components; rec {
-  components = efi en_us est extra gui kde steam docker;
+  components = efi en_us est extra gui kde steam docker home vfio;
 
   # flatpak
   services.flatpak.enable = true;
