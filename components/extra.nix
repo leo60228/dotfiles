e1b50c30e7c5842ad25cd2bc08031c0c102a0113
leo60228 <leo@60228.dev>
2019-05-25 15:30:56 -0400

    YubiKey

--- components/extra.nix
+++ components/extra.nix
@@ -7,9 +7,9 @@ lib.makeComponent "extra"
     security.sudo.wheelNeedsPassword = false;
 
     services.pcscd.enable = true;
-    
+
     hardware.enableAllFirmware = true;
-    
+
     # exfat
     #boot.extraModulePackages = [ config.boot.kernelPackages.exfat-nofuse ];
 
@@ -20,13 +20,13 @@ lib.makeComponent "extra"
     # printer
     services.printing.enable = true;
     services.printing.drivers = [ pkgs.hplip pkgs.gutenprint pkgs.gutenprintBin ];
-    
+
     environment.systemPackages = with pkgs; [
       wget vim qemu
     ];
-    
+
     programs.bash.enableCompletion = true;
-    
+
     networking.networkmanager.enable = true; # Enable NetworkManager to manage Wi-Fi connections
 
     services.avahi.enable = true;
@@ -42,6 +42,7 @@ lib.makeComponent "extra"
     services.udev.extraRules = ''
       SUBSYSTEM=="usb", ATTR{idVendor}=="0955", ATTR{idProduct}=="7321", MODE="0666"
       SUBSYSTEM=="usb", ATTR{idVendor}=="1209", ATTR{idProduct}=="8b00", MODE="0666"
+      KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="1050", ATTRS{idProduct}=="0113|0114|0115|0116|0120|0200|0402|0403|0406|0407|0410", TAG+="uaccess", GROUP="users", MODE="0666"
     '';
 
     # tmpfs
