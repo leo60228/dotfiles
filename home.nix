ae1d220c207ddb43e0577a47d38005373249ba96
leo60228 <leo@60228.dev>
2019-07-25 12:22:14 -0400

    Steal zenstates from acowley/dotfiles

--- home.nix
+++ home.nix
@@ -3,6 +3,7 @@
 let gmusicproxy = pkgs.callPackage ./gmusicproxy.nix {};
 in {
   home.packages = with pkgs; [
+    (callPackage ./zenstates.nix {})
     (callPackage ./unityenv.nix {})
     easytag
     cantata
