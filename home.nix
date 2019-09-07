38734c2f03761702e67f7ddc192a25ba99aa964f
leo60228 <leo@60228.dev>
2019-09-07 12:39:33 -0400

    Add lftp

--- home.nix
+++ home.nix
@@ -3,6 +3,7 @@
 let gmusicproxy = pkgs.callPackage ./gmusicproxy.nix {};
 in {
   home.packages = with pkgs; [
+    lftp
     (import <unstable> {}).dolphinEmuMaster
     (callPackage ./kflash.py {})
     alsaLib
