4a20a73bea0b435f9c1aa4ca2ab0abac5ec88dba
leo60228 <leo@60228.dev>
2019-11-26 17:42:37 -0500

    fix mesa

--- nixpkgs/xorg.nix
+++ nixpkgs/xorg.nix
@@ -1,8 +1,9 @@
 self: super: {
     xorg = super.xorg // {
         xf86videoamdgpu = (import <unstable> {}).xorg.xf86videoamdgpu.override {
-            inherit (self) stdenv pkgconfig fetchurl mesa libGL libdrm udev;
+            inherit (self) stdenv pkgconfig fetchurl libGL libdrm udev;
             inherit (self.xorg) xorgproto xorgserver;
+            mesa = (import /home/leo60228/nixpkgs-navi {}).mesa;
         };
     };
 }
