fcc7070cf184384a89342bb5265da45ed4172d18
leo60228 <leo@60228.dev>
2020-02-10 07:43:44 -0500

    Add WIP module for macOS VM

--- systems/leotop.nix
+++ systems/leotop.nix
@@ -3,7 +3,7 @@
 with import ../components; {
   imports = [ ../cachix.nix ];
 
-  components = efi en_us est gui kde { bluetooth = true; } docker steam extra home;
+  components = efi en_us est gui kde { bluetooth = true; } docker steam extra home macvm;
 
   networking.hostName = "leotop2"; # Define your hostname.
 
