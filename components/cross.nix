cb57853c147b2c5cb6d1fc161fae5cf154aa5c12
leo60228 <leo@60228.dev>
2020-08-18 20:02:36 -0400

    Add component for Nix cross builds

---
+++ components/cross.nix
@@ -0,0 +1,9 @@
+let lib = import ../lib; in
+lib.makeComponent "cross"
+({cfg, pkgs, lib, ...}: with lib; {
+  opts = {};
+
+  config = {
+    boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
+  };
+})
