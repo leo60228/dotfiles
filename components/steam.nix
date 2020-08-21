70e27ad32d81a4aa4ab5e1c2201004fad4017b8b
leo60228 <leo@60228.dev>
2020-08-21 08:17:09 -0400

    remove weird steam hack

--- components/steam.nix
+++ components/steam.nix
@@ -8,14 +8,8 @@ lib.makeComponent "steam"
     hardware.opengl.driSupport32Bit = true;
     hardware.opengl.setLdLibraryPath = true;
     hardware.pulseaudio.support32Bit = true;
-    environment.systemPackages = [ ((pkgs.newScope pkgs.steamPackages) <nixpkgs/pkgs/games/steam/chrootenv.nix> {
-      glxinfo-i686 = pkgs.pkgsi686Linux.glxinfo;
-      steam-runtime-wrapped-i686 =
-        if pkgs.stdenv.hostPlatform.system == "x86_64-linux"
-        then pkgs.pkgsi686Linux.steamPackages.steam-runtime-wrapped
-        else null;
-    }) ];
+    environment.systemPackages = with pkgs; [ steam ];
 
-    services.udev.extraRules = builtins.replaceStrings ["0660"] ["0666"] (builtins.readFile ../files/steam-input.rules);
+    hardware.steam-hardware.enable = true;
   };
 })
