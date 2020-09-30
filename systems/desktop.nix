5c8f86a5891f06f4fb5cb48f718b68e030311eb1
leo60228 <leo@60228.dev>
2020-09-30 16:40:28 -0400

    datablase

--- systems/desktop.nix
+++ systems/desktop.nix
@@ -7,4 +7,16 @@ with import ../components; rec {
 
   networking.firewall.allowedTCPPorts = [ 3000 34567 34568 ];
   networking.firewall.allowedUDPPorts = [ 4010 34567 34568 ];
+
+  services.postgresql = {
+    enable = true;
+    ensureUsers = [ {
+      name = "leo60228";
+      ensurePermissions = {
+        "DATABASE datablase" = "ALL PRIVILEGES";
+        "ALL TABLES IN SCHEMA public" = "ALL PRIVILEGES";
+      };
+    } ];
+    ensureDatabases = [ "datablase" ];
+  };
 }
