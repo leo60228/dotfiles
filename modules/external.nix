ffe7ed5f3ae93fba61b432386ea5617dbfff3c02
leo60228 <leo@60228.dev>
2020-11-14 18:34:18 -0500

    hm fixese

--- modules/external.nix
+++ modules/external.nix
@@ -1,5 +1,13 @@
-{ ... }: {
+{ lib, ... }:
+let nixosRelease = lib.trivial.release;
+    hmReleases = {
+      "20.03" = https://github.com/nix-community/home-manager/archive/release-20.03.tar.gz;
+      "20.09" = https://github.com/nix-community/home-manager/archive/release-20.09.tar.gz;
+      "21.03" = https://github.com/nix-community/home-manager/archive/master.tar.gz;
+    };
+    hmRelease = hmReleases.${nixosRelease};
+in {
   imports = [
-    "${builtins.fetchTarball https://github.com/rycee/home-manager/archive/master.tar.gz}/nixos"
+    "${builtins.fetchTarball hmRelease}/nixos"
   ];
 }
