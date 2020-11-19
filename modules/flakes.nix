c3abdb776a5ccb4847f628f0ff8342d1bcde19c0
leo60228 <leo@60228.dev>
2020-11-19 18:01:29 -0500

    Add Nixpkgs flake to nixPath

--- modules/flakes.nix
+++ modules/flakes.nix
@@ -7,5 +7,9 @@
       experimental-features = nix-command flakes
     '';
     registry.nixpkgs.flake = flakes.nixpkgs;
+    nixPath = lib.mkForce [
+      "nixpkgs=${flakes.nixpkgs}"
+      "/nix/var/nix/profiles/per-user/root/channels"
+    ];
   };
 }
