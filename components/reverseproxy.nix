316be8ed131752eda416d53ea7809f7e68d85567
leo60228 <leo@60228.dev>
2020-11-19 21:40:34 -0500

    Remove broken personal domain

--- components/reverseproxy.nix
+++ components/reverseproxy.nix
@@ -191,16 +191,6 @@ lib.makeComponent "reverseproxy"
                 '';
             };
           };
-          "***REMOVED***" = {
-            forceSSL = true;
-            enableACME = true;
-            acmeRoot = "/var/lib/acme/acme-challenge";
-            locations = {
-              "/" = {
-                extraConfig = "return 301 https://aws.leo60228.space$request_uri;";
-              };
-            };
-          };
           "blog.leo60228.space" = {
             forceSSL = true;
             enableACME = true;
