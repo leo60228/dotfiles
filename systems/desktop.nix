bd8d9f815ce854e0af2ce340ca2e4b247d4c15a9
leo60228 <leo@60228.dev>
2020-12-19 12:19:16 -0500

    wg

--- systems/desktop.nix
+++ systems/desktop.nix
@@ -3,10 +3,10 @@
 { config, pkgs, lib, ... }:
 
 with import ../components; rec {
-  components = efi en_us est extra gui { autoLogin = true; } kde steam docker home kvm vfio glances flatpak mqtt cross;
+  components = efi en_us est extra gui { autoLogin = true; } kde steam docker home kvm vfio glances flatpak mqtt cross wg { ip = "10.100.0.2"; } prometheus;
 
-  networking.firewall.allowedTCPPorts = [ 3000 34567 34568 ];
-  networking.firewall.allowedUDPPorts = [ 4010 34567 34568 ];
+  networking.firewall.allowedTCPPorts = [ 3000 34567 34568 22000 8010 ];
+  networking.firewall.allowedUDPPorts = [ 4010 34567 34568 21027 ];
 
   services.postgresql = {
     enable = true;
