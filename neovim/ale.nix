b45e4b54dc20e3e147001f0132b1fd91782ed1cf
leo60228 <leo@60228.dev>
2021-01-09 12:12:36 -0500

    update ALE

--- neovim/ale.nix
+++ neovim/ale.nix
@@ -1,15 +1,14 @@
 { vimUtils, fetchgit, stdenv }:
 vimUtils.buildVimPlugin {
   pname = "ale";
-  version = "git-2020-11-14";
-  patches = [ ../files/ale-codefix-hack.diff ];
+  version = "git-2020-12-28";
   src = fetchgit {
     url = "https://github.com/dense-analysis/ale.git";
-    rev = "48fe0dd4f629bb1282277ba8a6757a84c13a4dda";
-    sha256 = "100lqxxrh34s9a9wgdnd1b419zfsn0bba1z9im07sbbp80acc5l7";
+    rev = "7fca451cf9a3068efe5e93fcc4b5494d939245fb";
+    sha256 = "1g87lrf5g4alnax982nxfc5aa39mzsmh6zwrzcxhs0zpinm52vlc";
   };
   meta = {
-    homepage = https://github.com/dense-analysis/ale; 
+    homepage = https://github.com/dense-analysis/ale;
     maintainers = [ stdenv.lib.maintainers.leo60228 ];
   };
 }
