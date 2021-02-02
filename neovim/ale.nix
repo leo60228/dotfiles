4a7afa759eb774a64ccc460ef0f23ded1195b72a
leo60228 <leo@60228.dev>
2021-02-02 12:22:23 -0500

    use RA built-in clippy integration

--- neovim/ale.nix
+++ neovim/ale.nix
@@ -1,11 +1,11 @@
 { vimUtils, fetchgit, stdenv }:
 vimUtils.buildVimPlugin {
   pname = "ale";
-  version = "git-2020-12-28";
+  version = "git-2021-02-02";
   src = fetchgit {
     url = "https://github.com/dense-analysis/ale.git";
-    rev = "7fca451cf9a3068efe5e93fcc4b5494d939245fb";
-    sha256 = "1g87lrf5g4alnax982nxfc5aa39mzsmh6zwrzcxhs0zpinm52vlc";
+    rev = "9b5c09047361f3ec2cf18afbb6d1e03047a59778";
+    sha256 = "043jbgqw3m8bqsp1piamwgmsqg3136fhksagb157zppxbw25jkgd";
   };
   meta = {
     homepage = https://github.com/dense-analysis/ale;
