4326c368f86c4d89b375b4c94484273a7ed7a99c
leo60228 <leo@60228.dev>
2021-02-06 18:21:21 -0500

    Trust SSH CA

--- modules/base.nix
+++ modules/base.nix
@@ -16,6 +16,10 @@
     keys = builtins.filter (x: x != "") split;
   in keys;
 
+  services.openssh.extraConfig = ''
+  TrustedUserCAKeys ${../files/ssh-ca.pub}
+  '';
+
   # This value determines the NixOS release with which your system is to be
   # compatible, in order to avoid breaking some software such as database
   # servers. You should change this only after NixOS release notes say you
