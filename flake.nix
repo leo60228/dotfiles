daf99b9c9a2998b66636c162b44d5eeb12a618a6
leo60228 <leo@60228.dev>
2021-02-11 10:43:08 -0500

    add nixos-unstable input

--- flake.nix
+++ flake.nix
@@ -1,5 +1,6 @@
 {
   inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-20.09;
+  inputs.nixpkgs-unstable.url = github:NixOS/nixpkgs/nixos-unstable;
   inputs.home-manager = {
     url = github:nix-community/home-manager/release-20.09;
     inputs.nixpkgs.follows = "nixpkgs";
