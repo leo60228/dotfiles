0a0c479dbeddd62e7244aaf61890b0b0f2b7e247
leo60228 <leo@60228.dev>
2021-03-11 14:16:20 -0500

    update firefox

--- firefox.nix
+++ firefox.nix
@@ -1,13 +1,13 @@
 { lib }:
 lib.firefoxOverlay.firefoxVersion {
   name = "Firefox Beta";
-  version = "87.0b3";
+  version = "87.0b6";
   release = true;
   info = {
-    url = "https://download.cdn.mozilla.net/pub/firefox/releases/87.0b3/linux-x86_64/en-US/firefox-87.0b3.tar.bz2";
-    sha512 = "51fe015e54b648420ccfa4d43cd5cfb4b19765eb5a93fe525d81497c63cddb2c9a37f71366b50e0673fbe147c52e2bfef62f3241d30e6402c32946e3670114e7";
+    url = "https://download.cdn.mozilla.net/pub/firefox/releases/87.0b6/linux-x86_64/en-US/firefox-87.0b6.tar.bz2";
+    sha512 = "cb6971ae8e3169a2bf0a63bc13be910fe0bda4a779804fb2bcceebcbbbbcd3455137469ca3abcda4378c3468048f342f9f6b7b93df8b16aee54425b7a58eccb0";
     chksumSig = null;
-    sig = "https://download.cdn.mozilla.net/pub/firefox/releases/87.0b3/linux-x86_64/en-US/firefox-87.0b3.tar.bz2.asc";
-    sigSha512 = "d90d5332041762a84f8a4368410e18fb9e1adb465d20cadb908385ac6feefab8277d19864b17c4f5c4ac88bed2d25ab2037990374f11fe3609c483728bc35544";
+    sig = "https://download.cdn.mozilla.net/pub/firefox/releases/87.0b6/linux-x86_64/en-US/firefox-87.0b6.tar.bz2.asc";
+    sigSha512 = "9e91e4f5bca5c570433afac2bf99485979c537f917a8598898e1670b65196907be58a3a60be402ee56d4f95bebd784172251e764540d18845bd6f0990f603c43";
   };
 }
