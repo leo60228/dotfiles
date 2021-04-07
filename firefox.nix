7fdd030f62c63afc56e85d06c45d37a7176fd1fe
leo60228 <leo@60228.dev>
2021-04-07 16:16:33 -0400

    upd8 firefox to version 88 beta 8

--- firefox.nix
+++ firefox.nix
@@ -1,13 +1,13 @@
 { lib }:
 lib.firefoxOverlay.firefoxVersion {
   name = "Firefox Beta";
-  version = "88.0b2";
+  version = "88.0b8";
   release = true;
   info = {
-    url = "https://download.cdn.mozilla.net/pub/firefox/releases/88.0b2/linux-x86_64/en-US/firefox-88.0b2.tar.bz2";
-    sha512 = "cb712d765de8e7860b8625633b483f2202ef628b5cf5a116e91f38b9b49cfb8d6bf8a6ff347912137d4c24d6d89ec43c8c72eefc93b35e8e425d763861f85527";
+    url = "https://download.cdn.mozilla.net/pub/firefox/releases/88.0b8/linux-x86_64/en-US/firefox-88.0b8.tar.bz2";
+    sha512 = "0491b374451d217ba44b39fe03d32076258415ea9794c4428657bb885a7211805adc8ea0522334afde7598d5759735446e6e8bdc55f88b0b21fefc3ed1da7a71";
     chksumSig = null;
-    sig = "https://download.cdn.mozilla.net/pub/firefox/releases/88.0b2/linux-x86_64/en-US/firefox-88.0b2.tar.bz2.asc";
-    sigSha512 = "d51c5e0c4c5ab8f3cc2ddc438917c4d96cff8d556f57f2445028125e8cff29605a970a47129df12cbb6e7cf48f32616302b3efedebdf194434e2db15a54b4d05";
+    sig = "https://download.cdn.mozilla.net/pub/firefox/releases/88.0b8/linux-x86_64/en-US/firefox-88.0b8.tar.bz2.asc";
+    sigSha512 = "4187b6235f8ed0bf43833a71c2b277d8a08b15d0a4b8c2422bfb866c43f91bc087fe3edbfa3bdab340c469460ca760a8a16c2f791ef92b970fb7cb51100deb13";
   };
 }
