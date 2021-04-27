aea37fccbcdc6d456ec7e4322c5aaab30171cebc
leo60228 <leo@60228.dev>
2021-04-27 15:27:06 -0400

    tailscale leoserv

--- systems/leoserv.nix
+++ systems/leoserv.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }:
 
 with import ../components; {
-  components = efi en_us est home { small = true; } znc;
+  components = efi en_us est home { small = true; } tailscale znc;
 
   #networking.firewall.allowedTCPPorts = [ 25565 25575 38067 ];
   #networking.firewall.allowedUDPPorts = [ 25565 25575 38067 ];
@@ -43,4 +43,9 @@ with import ../components; {
   #  domains = [ "vsix-modded-mc" ];
   #  protocol = "duckdns";
   #};
+
+  networking.dhcpcd.extraConfig = ''
+  static domain_name_servers=100.100.100.100 79.110.170.43 1.1.1.1 1.0.0.1
+  static domain_search=60228.dev.beta.tailscale.net
+  '';
 }
