e6bba887afb03b3fc644207697cbac785f2eee01
leo60228 <leo@60228.dev>
2021-05-26 10:32:46 -0400

    multimc legacy fabric

--- nixpkgs/multimc.nix
+++ nixpkgs/multimc.nix
@@ -1,6 +1,6 @@
 self: super: {
     multimc = (self.libsForQt5.callPackage ../multimc.nix {}).overrideAttrs (oldAttrs: {
-        patches = [ ../files/multimc-png-logo.patch ];
+        patches = [ ../files/multimc-png-logo.patch ../files/multimc-legacy-fabric.patch ];
         postPatch = ''
         mkdir -v application/resources/multimc/512x512
         cp -v ${../files/multimc.png} application/resources/multimc/512x512/logo.png
