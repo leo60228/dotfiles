f4309744b602dfc77ee18ca91a3b7c2270ddfc35
leo60228 <leo@60228.dev>
2021-05-30 09:43:45 -0400

    Install GraalVM on leotop

--- systems/leotop.nix
+++ systems/leotop.nix
@@ -3,7 +3,7 @@
 with import ../components; {
   imports = [ ../cachix.nix ];
 
-  components = efi { removable = true; } en_us est gui kde { bluetooth = true; } docker steam extra home kvm tailscale;
+  components = efi { removable = true; } en_us est gui kde { bluetooth = true; } docker steam extra { graalvm = true; } home kvm tailscale flatpak;
 
   users.extraUsers.leo60228.extraGroups = [ "wheel" "docker" "bumblebee" "vboxusers" ];
 
