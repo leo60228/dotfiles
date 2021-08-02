52a577915dcbce9954215a18441824f73ec594df
leo60228 <leo@60228.dev>
2021-08-02 13:18:25 -0400

    pebble cache

--- modules/base.nix
+++ modules/base.nix
@@ -1,7 +1,7 @@
 { pkgs, lib, config, flakes, ... }:
 
 {
-  imports = [ ./xpra.nix ./flakes.nix ./nix-daemon.nix ];
+  imports = [ ./xpra.nix ./flakes.nix ./nix-daemon.nix ./cachix.nix ];
 
   users.extraUsers.leo60228 = {
     isNormalUser = true;
---
+++ modules/cachix/pebble.nix
@@ -0,0 +1,12 @@
+
+{
+  nix = {
+    binaryCaches = [
+      "https://pebble.cachix.org"
+    ];
+    binaryCachePublicKeys = [
+      "pebble.cachix.org-1:aTqwT2hR6lGggw/rPISRcHZctDv2iF7ewsVxf3Hq6ow="
+    ];
+  };
+}
+    
