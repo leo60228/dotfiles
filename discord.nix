ccac18f8d8c3cee9444e450a9f558b4ba9405215
leo60228 <leo@60228.dev>
2021-08-27 18:03:38 -0400

    upd8 discord

--- discord.nix
+++ discord.nix
@@ -17,10 +17,10 @@ in {
     pname = "discord-canary";
     binaryName = "DiscordCanary";
     desktopName = "Discord Canary";
-    version = "0.0.126";
+    version = "0.0.128";
     src = fetchurl {
       url = "https://dl-canary.discordapp.net/apps/linux/${version}/discord-canary-${version}.tar.gz";
-      sha256 = "0apj1c4my17ca452wdga2zb82iqsbljbbg2fylfdvslx286r7dhj";
+      sha256 = "18xd2mglb75wpc9cb8p4hlydl86gyd5g15585h84x9jsr421h3bk";
     };
   };
 }.${branch}
