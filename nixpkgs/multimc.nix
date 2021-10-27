2be0867e03cd68ee8198fcc4eb2a757ffa5f8b6e
leo60228 <leo@60228.dev>
2021-10-27 15:16:49 -0400

    upd8 multimc

--- nixpkgs/multimc.nix
+++ nixpkgs/multimc.nix
@@ -1,9 +1,26 @@
 self: super: {
-    multimc = (self.libsForQt5.callPackage ../multimc.nix {}).overrideAttrs (oldAttrs: {
-        patches = [ ../files/multimc-png-logo.patch ../files/multimc-legacy-fabric.patch ];
-        postPatch = ''
-        mkdir -v application/resources/multimc/512x512
-        cp -v ${../files/multimc.png} application/resources/multimc/512x512/logo.png
+    multimc = (self.libsForQt5.callPackage ../multimc.nix {
+        msaClientID =
+            let
+                inherit (self.lib) elemAt mod range lowerChars upperChars replaceStrings;
+                alphaRange = range 0 25;
+                rot13Index = x: mod (x + 13) 26;
+                rot13Lookup = alpha: x: elemAt alpha (rot13Index x);
+                makeRot13Alphabet = alpha: map (rot13Lookup alpha) alphaRange;
+                lowerRot13 = makeRot13Alphabet lowerChars;
+                upperRot13 = makeRot13Alphabet upperChars;
+                allChars = lowerChars ++ upperChars;
+                allRot13 = lowerRot13 ++ upperRot13;
+                rot13 = replaceStrings allChars allRot13;
+
+                rot13ClientID = "7q151180-o320-4341-852s-q746764r7r0q";
+            in
+                rot13 rot13ClientID;
+    }).overrideAttrs (oldAttrs: {
+        patches = oldAttrs.patches ++ [ ../files/multimc-png-logo.patch ];
+        postPatch = oldAttrs.postPatch + ''
+        mkdir -v launcher/resources/multimc/512x512
+        cp -v ${../files/multimc.png} launcher/resources/multimc/512x512/logo.png
         '';
     });
 }
