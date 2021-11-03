ce4294c6517bd4e04e3962dc16d2d844ce154d93
leo60228 <leo@60228.dev>
2021-11-03 10:01:07 -0400

    open ports

--- systems/desktop.nix
+++ systems/desktop.nix
@@ -5,7 +5,7 @@
 with import ../components; rec {
   components = efi en_us est extra { graalvm = true; } gui { autoLogin = true; } kde steam docker home { deviceScaleFactor = 2; } kvm glances flatpak mqtt wg prometheus anbox ibus apcupsd { timeout = 300; minutes = 15; batteryLevel = 50; prometheus = "10.100.0.2"; } tpm2 tailscale postgres mosh usbmuxd nixbuild;
 
-  networking.firewall.allowedTCPPorts = [ 3000 34567 34568 22000 8010 6600 ];
+  networking.firewall.allowedTCPPorts = (lib.range 3000 3010) ++ [ 34567 34568 22000 8010 6600 ];
   networking.firewall.allowedUDPPorts = [ 4010 34567 34568 21027 6600 ];
 
   networking.hosts."52.218.200.91" = [ "www.blaseball2.com" ];
