1c2a475c911e1ec58382148fee956a4a003fc012
leo60228 <leo@60228.dev>
2021-12-08 14:59:17 -0500

    update mosh postPatch

--- nixpkgs/mosh.nix
+++ nixpkgs/mosh.nix
@@ -14,5 +14,11 @@ super:
             rev = "e023e81c08897c95271b4f4f0726ec165bb6e1bd";
             sha256 = "X2xJCiC5/vSijzZgQsWDzD+R8D8ppdZD6WeG4uoxyYw=";
         };
+
+        postPatch = ''
+            substituteInPlace scripts/mosh.pl \
+                --subst-var-by ssh "${self.openssh}/bin/ssh" \
+                --subst-var-by mosh-client "$out/bin/mosh-client"
+        '';
     });
 }
