5c4b9d64a1cd6bcf38f6803e6e494a9d4f5486a7
leo60228 <leo@60228.dev>
2021-12-27 18:44:27 -0500

    1080p zephyrus

--- hardware/zephyrus.nix
+++ hardware/zephyrus.nix
@@ -12,20 +12,20 @@
         boot.extraModulePackages = [ ];
 
         fileSystems."/" =
-        { device = "/dev/disk/by-uuid/0488d275-44c7-427d-ada9-7b9ff86fb918";
+        { device = "/dev/disk/by-uuid/b1a22376-6513-4815-b98d-9a1b6a6069a4";
             fsType = "btrfs";
         };
 
         fileSystems."/boot" =
-        { device = "/dev/disk/by-uuid/A6ED-2C41";
+        { device = "/dev/disk/by-uuid/4210-B02C";
             fsType = "vfat";
         };
 
         swapDevices =
-            [ { device = "/dev/disk/by-uuid/c5bdf63b-95a3-42fd-8168-ef719035f50b"; }
+            [ { device = "/dev/disk/by-uuid/2ebfa1d0-e726-4227-9365-53b66e7d5a21"; }
             ];
 
-        boot.initrd.luks.devices."encrypted".device = "/dev/disk/by-uuid/a799c51a-f91e-4bb9-b5bc-de915756e589";
+        boot.initrd.luks.devices."encrypted".device = "/dev/disk/by-uuid/26c8fcc6-0823-472d-afdb-61d823af227c";
 
         hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 
@@ -36,13 +36,6 @@
             amdgpuBusId = "PCI:4:0:0";
             nvidiaBusId = "PCI:1:0:0";
         };
-
-        # high-resolution display
-        hardware.video.hidpi.enable = lib.mkDefault true;
-        services.xserver.displayManager.xserverArgs = [ "-dpi 128" ];
-        console.earlySetup = true;
-        console.packages = [ pkgs.terminus_font ];
-        console.font = "ter-v32n";
     };
 
     nixops = {
