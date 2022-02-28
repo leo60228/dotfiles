067e9b42f4c0901e21e6de8921819029ac641868
leo60228 <leo@60228.dev>
2022-02-27 20:40:15 -0500

    upd8 firefox

--- firefox.nix
+++ firefox.nix
@@ -1,12 +1,12 @@
 lib: lib.firefoxOverlay.firefoxVersion {
   name = "Firefox Beta";
-  version = "98.0b3";
+  version = "98.0b9";
   release = true;
   info = {
-    url = "https://download.cdn.mozilla.net/pub/firefox/releases/98.0b3/linux-x86_64/en-US/firefox-98.0b3.tar.bz2";
-    sha512 = "dd147d0f329d29412cf7018902c282203ce5fe4f8febbd21c8e9a682180ae70f9d1a2a3d8a3640fef778a663840b8bc7dd9da295f5d196d8dbd55f1f73e139be";
+    url = "https://download.cdn.mozilla.net/pub/firefox/releases/98.0b9/linux-x86_64/en-US/firefox-98.0b9.tar.bz2";
+    sha512 = "7da10021dd80b996bf1cfd338688b0d725029566f72d28a8b2aa73bcc000625b5a540663570c4e321047f242b4d3c73c57f8a7d4287382449f358113ed8edb9a";
     chksumSig = null;
-    sig = "https://download.cdn.mozilla.net/pub/firefox/releases/98.0b3/linux-x86_64/en-US/firefox-98.0b3.tar.bz2.asc";
-    sigSha512 = "8bb7eee665109139ce4bf31c445d22722b9610627ef227fe94cfae8cfb47d7016c10a69052bdefecf3c84ae47ea2b6d94cca46a56e682d76226bc5a8cc2bddaa";
+    sig = "https://download.cdn.mozilla.net/pub/firefox/releases/98.0b9/linux-x86_64/en-US/firefox-98.0b9.tar.bz2.asc";
+    sigSha512 = "868ba61683e5b5b016be61e36ec8e7495482486416dc3901058386801e8c60dde23c50647372a2ee50c6f903843c2c0cb11346818949a837be43f4bee9063dde";
   };
 }
