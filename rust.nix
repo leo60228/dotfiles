76c8059a9ad912a4bd7920c03a18455b2fd3bc15
leo60228 <leo@60228.dev>
2022-06-16 14:24:19 -0400

    upd8 rust

--- rust.nix
+++ rust.nix
@@ -3,8 +3,8 @@
 rec {
   channel = rustChannelOf {
     channel = "nightly";
-    date = "2022-02-05";
-    sha256 = "Nm7sdmI3BxLdJMt26EOz8IUnzbw+KZD89hL87F+lYng=";
+    date = "2022-06-14";
+    sha256 = "6SImTPmS291ds3SzW+lalI6ZA3COgJbr/CizSXq71HM=";
   };
   rust = channel.rust.override {
     extensions = [ "clippy-preview" "rust-src" "rust-analysis" ];
@@ -21,6 +21,7 @@ rec {
       "wasm32-unknown-unknown"
       "x86_64-unknown-linux-musl"
       "riscv64gc-unknown-none-elf"
+      "riscv32imc-unknown-none-elf"
     ];
   };
 }
