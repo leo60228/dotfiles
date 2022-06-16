1806aa0b2d417606ff2760025c153117c3c63989
leo60228 <leo@60228.dev>
2022-06-16 14:41:08 -0400

    remove ancient julia packages

--- julia-nixpkgs.nix
+++
@@ -1,4 +0,0 @@
-import ./pin.nix {
-  rev = "4445414073205b5e282e570abbc0931ccacece60";
-  sha256 = "1530scy9w7bk141cfhb6y02bv783c83wq2wgw9ndzz97gy0lr969";
-}
--- julia-oldpkgs.nix
+++
@@ -1,8 +0,0 @@
-{pkgs ? import <nixpkgs> {}, version ? "10"}:                                                                                                                                                
-if version == "06" then import ./julia-wrapped.nix {                                                                                                                                          
-  inherit pkgs;                                                                                                                                                                               
-  juliapkg = (import ./julia-nixpkgs.nix).julia_06;                                                                                                                                           
-} else import ./julia-wrapped.nix {                                                                                                                                                           
-  inherit pkgs;                                                                                                                                                                               
-  juliapkg = pkgs."julia_${version}";                                                                                                                                                         
-}                                                                                                                                                                                             
