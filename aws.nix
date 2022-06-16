5a6b6989bbfc6ac7993dc06e2e6ef3ea299faf26
leo60228 <leo@60228.dev>
2022-06-16 14:36:09 -0400

    remove aws.nix

--- aws.nix
+++
@@ -1,1 +0,0 @@
-import ./rawConfig.nix "aws"
--- rebuild-aws.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-cd "$(dirname $0)"
-NIXOS_CONFIG="$(pwd)/aws.nix" nixos-rebuild --build-host root@ghostnet.leo60228.space --target-host root@ghostnet.leo60228.space "$@"
