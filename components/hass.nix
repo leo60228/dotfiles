55ca45eff12b598848f0164155aae2abe6e544c2
leo60228 <leo@60228.dev>
2022-07-08 17:25:19 -0400

    more hass

--- components/hass.nix
+++ components/hass.nix
@@ -10,7 +10,27 @@ lib.makeComponent "hass"
       enable = true;
       config = null;
       package = pkgs.home-assistant.override {
-	extraComponents = [ "default_config" "met" "hue" "ipp" "cast" "serial" "mqtt" "unifi" ];
+	extraComponents = [ "default_config" "met" "hue" "ipp" "cast" "serial" "mqtt" "unifi" "nut" "backup" ];
+      };
+    };
+
+    services.zigbee2mqtt = {
+      enable = true;
+      settings = {
+	homeassistant = true;
+	serial.port = "/dev/serial/by-id/usb-Texas_Instruments_TI_CC2531_USB_CDC___0X00124B001949B2CD-if00";
+	frontend.port = 8456;
+	advanced.network_key = "!secret network_key";
+	mqtt.server = "mqtt://10.4.13.1";
+      };
+    };
+
+    power.ups = {
+      enable = true;
+      mode = "netserver";
+      ups.apcupsd = {
+	driver = "apcupsd-ups";
+	port = "10.4.13.1";
       };
     };
   };
