372edc060f433b0221ef3543532018b563e920ea
leo60228 <leo@60228.dev>
2022-07-13 21:10:39 -0400

    klipper

--- systems/leopine.nix
+++ systems/leopine.nix
@@ -1,4 +1,4 @@
 { config, pkgs, lib, ... }:
 with import ../components; rec {
-  components = efi { removable = true; } en_us est home { small = true; } tailscale;
+  components = efi { removable = true; } en_us est home { small = true; } tailscale klipper;
 }
