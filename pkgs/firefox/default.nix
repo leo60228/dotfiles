0c577de8747857df509df41fca37ea642ebcf47d
leo60228 <leo@60228.dev>
2022-07-26 21:02:53 -0400

    upd8 firefox

--- pkgs/firefox/default.nix
+++ pkgs/firefox/default.nix
@@ -1,13 +1,13 @@
 { lib }: lib.firefoxOverlay.firefoxVersion {
   name = "Firefox Beta";
   wmClass = "firefox-beta";
-  version = "103.0b9";
+  version = "104.0b2";
   release = true;
   info = {
-    url = "https://download.cdn.mozilla.net/pub/firefox/releases/103.0b9/linux-x86_64/en-US/firefox-103.0b9.tar.bz2";
-    sha512 = "cc668fc0ccf2029afb2640fa955ceefca24907dc3fae472cc3b24865d071cd03f3d22e017b769f1c675f077f67f35bf81ec1304fbc930b131b19fbd0b2407c35";
+    url = "https://download.cdn.mozilla.net/pub/firefox/releases/104.0b2/linux-x86_64/en-US/firefox-104.0b2.tar.bz2";
+    sha512 = "4d38d33cfa1362e6eae1f7ae4352b52e088eb67ea81886edc15225d5058877271023195000d286801064892d6a89b264b59c8ae3a550389c0465184ffaf760e2";
     chksumSig = null;
-    sig = "https://download.cdn.mozilla.net/pub/firefox/releases/103.0b9/linux-x86_64/en-US/firefox-103.0b9.tar.bz2.asc";
-    sigSha512 = "bf7d3d5dc6c0ef2298efcfabb01955731ddee8aa5a32c092d4bbbeed96c43d50997b61b4b6e21e51606f2089394d2814565487f847ea8fe08bc8a510424aa6e6";
+    sig = "https://download.cdn.mozilla.net/pub/firefox/releases/104.0b2/linux-x86_64/en-US/firefox-104.0b2.tar.bz2.asc";
+    sigSha512 = "2c7416d68b3c396840045ab9a5a45b92f4eaf8c3746f084ad5a6d01c652be8aa66bf7ecc3aafbd7ceb02be1e724dab1acdba139bcecbe5fb0257071e7b947b9e";
   };
 }
