e06520e6115d5e2480376c4644ba1c150783b2a3
leo60228 <leo@60228.dev>
2022-07-26 21:02:00 -0400

    lxd

---
+++ components/lxd.nix
@@ -0,0 +1,10 @@
+let lib = import ../lib; in
+lib.makeComponent "lxd"
+({cfg, pkgs, lib, ...}: with lib; {
+  opts = {};
+
+  config = {
+    virtualisation.lxd.enable = true;
+    users.extraUsers.leo60228.extraGroups = [ "lxd" ];
+  };
+})
