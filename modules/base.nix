65bafa31f02b8d558d518f2ce312a6c622025587
leo60228 <leo@60228.dev>
2023-01-26 16:57:14 -0500

    desktop substituter

--- modules/base.nix
+++ modules/base.nix
@@ -38,6 +38,9 @@
   # trusted users
   nix.settings.trusted-users = [ "root" "@wheel" ];
   nix.settings.allowed-uris = [ "https://github.com" "https://gitlab.com" ];
+  nix.settings.trusted-public-keys = [ "desktop-1:jpWiJK7Ltbcf1b9xr9mx/4on1NqqmqTZG4bldBl41oQ=" ];
+  nix.settings.substituters = if config.networking.hostName == "desktop" then [] else [ "http://desktop:9999" ];
+  nix.settings.trusted-substituters = if config.networking.hostName == "desktop" then [] else [ "http://desktop:9999" ];
 
   environment.systemPackages = with pkgs; [
     openssh git
