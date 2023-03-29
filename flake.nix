155d55e7cda6e694b1c26cebff0f38265a7b597b
leo60228 <leo@60228.dev>
2023-03-29 14:50:09 -0400

    upd8 to unstable

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
-  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-22.11;
+  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
   inputs.home-manager = {
-    url = github:nix-community/home-manager/release-22.11;
+    url = github:nix-community/home-manager;
     inputs.nixpkgs.follows = "nixpkgs";
   };
   inputs.nixpkgs-mozilla = {
--- pkgs/firefox/default.nix
+++ pkgs/firefox/default.nix
@@ -1,13 +1,13 @@
 { lib }: lib.firefoxOverlay.firefoxVersion {
   name = "Firefox Beta";
   wmClass = "firefox-beta";
-  version = "112.0b4";
+  version = "112.0b8";
   release = true;
   info = {
-    url = "https://download.cdn.mozilla.net/pub/firefox/releases/112.0b4/linux-x86_64/en-US/firefox-112.0b4.tar.bz2";
-    sha512 = "ee6e6cdd9dee8d97348da8ff2848b9b33ccd554790a66e90871b1e4103f963860ff9ab535f3349470b40c527c9ffbc292b1b2b765d6d6249d2702e0d3aa05407";
+    url = "https://download.cdn.mozilla.net/pub/firefox/releases/112.0b8/linux-x86_64/en-US/firefox-112.0b8.tar.bz2";
+    sha512 = "eb698a35bbc2c67905df4da7045fe53f7477c9465c6dfa154b5d14af3646b560b13dd78eb82e201046b52c86b1db9c06d9a0298a359d2c1775eb3dfcc72e300d";
     chksumSig = null;
-    sig = "https://download.cdn.mozilla.net/pub/firefox/releases/112.0b4/linux-x86_64/en-US/firefox-112.0b4.tar.bz2.asc";
-    sigSha512 = "52d4d4954fd1f97b5cbd4fc126a6f5f19298cc9d4e698de41787412de1a83660341224570035b1554cf7cd9643532894dfb9eca1449aff22fd6349dc96e2986d";
+    sig = "https://download.cdn.mozilla.net/pub/firefox/releases/112.0b8/linux-x86_64/en-US/firefox-112.0b8.tar.bz2.asc";
+    sigSha512 = "55828479b25ce5f2890999ec1972936c16a0f912d6f0cf692ebccb20f09184ff574f76542cbd667c0540315413146f4653a84afd3f7dea76548a83a1f6a03bd0";
   };
 }
