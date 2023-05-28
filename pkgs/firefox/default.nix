7e637f50d2e743c5b21c355bf789702a630684e8
leo60228 <leo@60228.dev>
2023-05-28 13:18:33 -0400

    upd8 firefox

--- pkgs/firefox/default.nix
+++ pkgs/firefox/default.nix
@@ -1,13 +1,13 @@
 { lib }: lib.firefoxOverlay.firefoxVersion {
   name = "Firefox";
   wmClass = "firefox";
-  version = "113.0.1";
+  version = "113.0.2";
   release = true;
   info = {
-    url = "https://download.cdn.mozilla.net/pub/firefox/releases/113.0.1/linux-x86_64/en-US/firefox-113.0.1.tar.bz2";
-    sha512 = "2e1df39911a92a417c42c4d2733c833558858bba5c8216b5fc4cf82a920c9dc6a8bc629cf9fb92562065799aea6423f0126972e550070960f69a21205d072afb";
+    url = "https://download.cdn.mozilla.net/pub/firefox/releases/113.0.2/linux-x86_64/en-US/firefox-113.0.2.tar.bz2";
+    sha512 = "e68ef6551d7ad3231e29f445de8f526d6f6b196d86b2fccb7be03497cf8a6b67c202693d34ef117fab4eb78ab3897992e37cb421dd8abe23d3fcbe8e5dcea245";
     chksumSig = null;
-    sig = "https://download.cdn.mozilla.net/pub/firefox/releases/113.0.1/linux-x86_64/en-US/firefox-113.0.1.tar.bz2.asc";
-    sigSha512 = "2e8f301915e86cab4f881824b28e42f42b59f58e145f18f175e0416449cb0dce78b42a63ef1400a7f3c9f01ffb9323acf2ebd7cf27a094fb9dc281a0541d34ce";
+    sig = "https://download.cdn.mozilla.net/pub/firefox/releases/113.0.2/linux-x86_64/en-US/firefox-113.0.2.tar.bz2.asc";
+    sigSha512 = "b6e2666279eaa1d487a23ee46f199d8c26595e43e36bd42b69b2aa5c7d12c70519c8543299a4f13fa44c2c050ba278e319da9fe9460cdceaf546c476f18fe6a9";
   };
 }
