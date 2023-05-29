f5f0a61f2e5cffd391c104ac969e77fc058d0738
leo60228 <leo@60228.dev>
2023-05-29 16:17:03 -0400

    nvme in leoserv initrd

--- hardware/leoserv.nix
+++ hardware/leoserv.nix
@@ -3,7 +3,7 @@
   {
     imports = [ "${modulesPath}/installer/scan/not-detected.nix" ];
 
-    boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "usb_storage" "sd_mod" ];
+    boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usb_storage" "sd_mod" ];
     boot.initrd.kernelModules = [ "dm-snapshot" ];
     boot.kernelModules = [ "kvm-amd" ];
     boot.extraModulePackages = [ ];
