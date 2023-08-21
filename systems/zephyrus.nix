31092ee824589aac37656be5a75c76806e89764d
leo60228 <leo@60228.dev>
2023-08-21 14:11:53 -0400

    zephyrus kdeconnect

--- systems/zephyrus.nix
+++ systems/zephyrus.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }:
 
 with import ../components; {
-  components = efi en_us est gui kde { bluetooth = true; } docker steam extra home tailscale firefox flatpak fwupd;
+  components = efi en_us est gui kde { bluetooth = true; } docker steam extra home tailscale firefox flatpak fwupd kdeconnect;
 
   users.extraUsers.leo60228.extraGroups = [ "wheel" "docker" "bumblebee" "vboxusers" ];
 }
