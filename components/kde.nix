241ac2f50cf48a6dcb6f8ff028e45bb6583cfba2
leo60228 <leo@60228.dev>
2023-08-25 18:11:46 -0400

    yeet latte

--- components/kde.nix
+++ components/kde.nix
@@ -10,7 +10,7 @@ lib.makeComponent "kde"
 
   config = mkMerge [ {
     environment.systemPackages = with pkgs; [
-      plasma-nm plasma-pa plasma5Packages.kde-gtk-config latte-dock
+      plasma-nm plasma-pa plasma5Packages.kde-gtk-config
     ];
 
     # Enable the KDE Desktop Environment.
