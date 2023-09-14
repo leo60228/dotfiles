b6ed16b242de159babd9f8426db4fd9c2e617c13
leo60228 <leo@60228.dev>
2023-09-14 13:54:34 -0400

    rm nixpi

--- systems/nixpi.nix
+++
@@ -1,9 +0,0 @@
-# includes = ../rawConfig.nix:../hardware/desktop.nix:../components/{efi,en_us,est,extra,gui,kde,steam,docker,home,vfio}.nix
-
-{ config, pkgs, lib, ... }:
-
-with import ../components; rec {
-  components = en_us est home { small = true; } router hass unifi apcupsd-server mqtt tailscale;
-
-  services.openssh.settings.PasswordAuthentication = false;
-}
