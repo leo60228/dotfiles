c8c03275db4a18429005a81d0a000aa5130d17b6
leo60228 <leo@60228.dev>
2023-11-01 18:47:11 -0400

    fix crabstodon

--- pkgs/crabstodon/default.nix
+++ pkgs/crabstodon/default.nix
@@ -1,16 +1,10 @@
 { lib, mastodon, callPackage, fetchYarnDeps }:
 
 let src = callPackage ./source.nix {};
-in (mastodon.override {
+in mastodon.override {
   pname = "crabstodon";
-  version = ./version.nix;
+  version = import ./version.nix;
   srcOverride = src;
-  dependenciesDir = ./.;
-}).overrideAttrs (oldAttrs: {
-  mastodonModules = oldAttrs.mastodonModules.overrideAttrs (oldAttrs: {
-    yarnOfflineCache = fetchYarnDeps {
-      yarnLock = "${src}/yarn.lock";
-      sha256 = "sha256-WsPNqV1PC2YjL37qnWfRTj8LaIBUI7+C0cWTfFd7HGo=";
-    };
-  });
-})
+  gemset = ./. + "/gemset.nix";
+  yarnHash = "sha256-WsPNqV1PC2YjL37qnWfRTj8LaIBUI7+C0cWTfFd7HGo=";
+}
