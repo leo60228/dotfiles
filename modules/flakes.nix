a88d3e5f0925dd5d10e2d1f13d0309af02502d14
leo60228 <leo@60228.dev>
2024-05-12 18:50:20 -0400

    nixos 24.05 handles flake registry properly by default

--- modules/flakes.nix
+++ modules/flakes.nix
@@ -1,19 +1,5 @@
 { pkgs, lib, flakes, ... }:
 
 {
-  nix = {
-    extraOptions = ''
-      experimental-features = nix-command flakes
-    '';
-    registry.nixpkgs.to = {
-      type = "github";
-      owner = "NixOS";
-      repo = "nixpkgs";
-      inherit (flakes.nixpkgs) rev;
-    };
-    nixPath = lib.mkForce [
-      "nixpkgs=${flakes.nixpkgs}"
-      "/nix/var/nix/profiles/per-user/root/channels"
-    ];
-  };
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
 }
