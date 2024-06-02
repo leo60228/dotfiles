120b08e742f90f7988475cff63f44f7cec1a83a6
leo60228 <leo@60228.dev>
2024-06-02 16:27:16 -0400

    upd8 vim-poryscript

--- neovim/vim-poryscript.nix
+++ neovim/vim-poryscript.nix
@@ -1,11 +1,11 @@
 { vimUtils, fetchgit, stdenv, lib }:
 vimUtils.buildVimPlugin {
   pname = "poryscript";
-  version = "git-2024-06-01";
+  version = "git-2024-06-02";
   src = fetchgit {
     url = "https://gitlab.com/vriska/vim-poryscript.git";
-    rev = "15906beb7e5e34e39644ab1ff5905d72e6a58d6a";
-    sha256 = "13cir82y9f5racdyaqbcj5n2bqzzi3rxisajr0h95siig4vf3gri";
+    rev = "8e8325fb12e09d82414e532f2881a730b02a0b89";
+    sha256 = "0fmzk6il9rpz1ncwnizll996j5zdx4dgccl0s70i7fvyxq6w413b";
   };
   meta = {
     homepage = https://gitlab.com/vriska/vim-poryscript;
