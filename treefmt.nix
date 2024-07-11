db3f8724c06adb692e598e05136a84ce9ed79916
leo60228 <leo@60228.dev>
2024-07-11 13:52:40 -0400

    more treefmt formatters

--- treefmt.nix
+++ treefmt.nix
@@ -3,4 +3,43 @@
   projectRootFile = "flake.nix";
 
   programs.nixfmt.enable = true;
+  programs.yamlfmt.enable = true;
+  programs.jsonfmt.enable = true;
+
+  programs.shfmt = {
+    enable = true;
+    indent_size = null;
+  };
+
+  programs.mdformat = {
+    enable = true;
+    package = pkgs.mdformat.withPlugins (p: [ p.mdformat-gfm ]);
+  };
+
+  settings.formatter.shfmt.includes = [
+    "scripts/*"
+    "pkgs/bin/$"
+    "pkgs/bin/ahorn"
+    "pkgs/bin/all-static-gcc"
+    "pkgs/bin/celeste"
+    "pkgs/bin/chln"
+    "pkgs/bin/copy-screenshot"
+    "pkgs/bin/crops"
+    "pkgs/bin/devkita64"
+    "pkgs/bin/elixiremanager"
+    "pkgs/bin/elixireshorten"
+    "pkgs/bin/git-update-fork"
+    "pkgs/bin/herolab"
+    "pkgs/bin/make_fcp_x3g"
+    "pkgs/bin/mounthdd"
+    "pkgs/bin/nspawn"
+    "pkgs/bin/run_scaled"
+    "pkgs/bin/tasmota"
+    "pkgs/bin/vim"
+  ];
+
+  settings.global.excludes = [
+    "LICENSE"
+    ".git-blame-ignore-revs"
+  ];
 }
