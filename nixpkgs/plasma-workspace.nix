9f996ebd38277134ef8ad1df1d62f5ea569ff63a
leo60228 <leo@60228.dev>
2024-09-01 20:06:48 -0400

    fmt

--- nixpkgs/plasma-workspace.nix
+++ nixpkgs/plasma-workspace.nix
@@ -1,9 +1,11 @@
 self: super:
 
 {
-  kdePackages = super.kdePackages.overrideScope (self': super': {
-    plasma-workspace = super'.plasma-workspace.overrideAttrs (oldAttrs: {
-      patches = oldAttrs.patches ++ [ ../files/fix-plasma-qalculate.patch ];
-    });
-  });
+  kdePackages = super.kdePackages.overrideScope (
+    self': super': {
+      plasma-workspace = super'.plasma-workspace.overrideAttrs (oldAttrs: {
+        patches = oldAttrs.patches ++ [ ../files/fix-plasma-qalculate.patch ];
+      });
+    }
+  );
 }
