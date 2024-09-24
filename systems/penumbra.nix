9af16e563cc1ab605db8b41053268f57d62dc87b
leo60228 <leo@60228.dev>
2024-09-23 20:15:41 -0400

    penumbra libvirt

--- systems/penumbra.nix
+++ systems/penumbra.nix
@@ -9,7 +9,7 @@ with import ../components;
 {
   components = efi en_us est gui kde {
     bluetooth = true;
-  } docker steam extra home { deviceScaleFactor = 2; } tailscale firefox flatpak fwupd kdeconnect;
+  } docker steam extra home { deviceScaleFactor = 2; } tailscale firefox flatpak fwupd kdeconnect kvm;
 
   users.extraUsers.leo60228.extraGroups = [
     "wheel"
