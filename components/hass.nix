ba623dcaabfcac58628c1991ae4a560ff9592172
leo60228 <leo@60228.dev>
2024-11-17 23:16:22 -0500

    Revert "home-assistant-matter-hub"
    This reverts commit 8e07e2a5d1608e9e8f30b88dc5e0cb88f54d3994.

--- components/hass.nix
+++ components/hass.nix
@@ -16,38 +16,21 @@ lib.makeComponent "hass" (
       networking.firewall.allowedTCPPorts = [
         8123
         21063
-        8482
-        5540
       ];
-      networking.firewall.allowedUDPPorts = [
-        5353
-        5540
-      ];
+      networking.firewall.allowedUDPPorts = [ 5353 ];
 
       virtualisation.oci-containers = {
         backend = "podman";
-        containers = {
-          homeassistant = {
-            volumes = [ "home-assistant:/config" ];
-            environment.TZ = "America/New_York";
-            environment.PYTHONPATH = "/config/deps"; # https://github.com/home-assistant/core/issues/127966
-            image = "ghcr.io/home-assistant/home-assistant:stable";
-            extraOptions = [
-              "--privileged"
-              "--network=host"
-              "--pull=newer"
-            ];
-          };
-          hamh = {
-            volumes = [ "home-assistant-matter-hub:/data" ];
-            environment.TZ = "America/New_York";
-            environmentFiles = [ "/var/lib/hamh-secrets" ];
-            image = "ghcr.io/t0bst4r/home-assistant-matter-hub:latest";
-            extraOptions = [
-              "--network=host"
-              "--pull=newer"
-            ];
-          };
+        containers.homeassistant = {
+          volumes = [ "home-assistant:/config" ];
+          environment.TZ = "America/New_York";
+          environment.PYTHONPATH = "/config/deps"; # https://github.com/home-assistant/core/issues/127966
+          image = "ghcr.io/home-assistant/home-assistant:stable";
+          extraOptions = [
+            "--privileged"
+            "--network=host"
+            "--pull=newer"
+          ];
         };
       };
 
