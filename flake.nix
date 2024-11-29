d13b9f562a3e9ae2d5b3d163e1359fb25334edcf
leo60228 <leo@60228.dev>
2024-11-29 17:43:22 -0500

    upd8 nix-eval-jobs

--- flake.nix
+++ flake.nix
@@ -42,7 +42,7 @@
     inputs.nix-eval-jobs.follows = "nix-eval-jobs";
   };
   inputs.nix-eval-jobs = {
-    url = "https://git.lix.systems/lix-project/nix-eval-jobs/archive/main.tar.gz";
+    url = "https://git.lix.systems/vriska/nix-eval-jobs/archive/update-lix.tar.gz";
     inputs.nixpkgs.follows = "nixpkgs";
     inputs.lix.follows = "lix";
   };
