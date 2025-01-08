eaa4c3e865a9e97973b8cb3a2e806aef270ebb32
leo60228 <leo@60228.dev>
2025-01-07 20:10:36 -0500

    don't override sddm scaling

--- hardware/desktop.nix
+++ hardware/desktop.nix
@@ -178,7 +178,6 @@
       services.displayManager.sddm.settings = {
         General = {
           Numlock = "none";
-          GreeterEnvironment = "QT_SCREEN_SCALE_FACTORS=2,QT_FONT_DPI=192";
         };
 
         Theme = {
