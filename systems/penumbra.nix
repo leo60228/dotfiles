a1cfee1833cb937142d1194d566b47c139dce613
leo60228 <leo@60228.dev>
2025-03-14 21:09:29 -0400

    lxc on penumbra

--- systems/penumbra.nix
+++ systems/penumbra.nix
@@ -11,13 +11,46 @@ with import ../components;
     bluetooth = true;
   } docker steam extra home { deviceScaleFactor = 2; } tailscale flatpak fwupd kdeconnect kvm;
 
-  users.extraUsers.leo60228.extraGroups = [
-    "wheel"
-    "docker"
-  ];
+  users.extraUsers.leo60228 = {
+    extraGroups = [
+      "wheel"
+      "docker"
+      "lxc-user"
+    ];
+    createHome = false;
+    autoSubUidGidRange = lib.mkForce false;
+    subUidRanges = [
+      {
+        startUid = 1000;
+        count = 1;
+      }
+      {
+        startUid = 100000;
+        count = 65536 * 2;
+      }
+    ];
+    subGidRanges = [
+      {
+        startGid = 100;
+        count = 1;
+      }
+      {
+        startGid = 100000;
+        count = 65536 * 2;
+      }
+    ];
+  };
 
   networking.firewall.allowedTCPPorts = lib.range 3000 3010;
   networking.firewall.allowedUDPPorts = lib.range 3000 3010;
 
   virtualisation.waydroid.enable = true;
+
+  virtualisation.lxc = {
+    enable = true;
+    unprivilegedContainers = true;
+    usernetConfig = ''
+      leo60228 veth lxcbr0 10
+    '';
+  };
 }
