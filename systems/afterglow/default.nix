b175f83e6af54f3aecaad01c649f972e436765dc
leo60228 <leo@60228.dev>
2025-04-21 17:29:04 -0400

    osk on afterglow

--- systems/afterglow/default.nix
+++ systems/afterglow/default.nix
@@ -11,4 +11,19 @@
   system.stateVersion = "25.05";
 
   vris.graphical = true;
+
+  services.displayManager.sessionPackages = [ pkgs.kdePackages.plasma-mobile ];
+  services.displayManager.sddm.wayland.compositorCommand = lib.concatStringsSep " " [
+    "${lib.getBin pkgs.kdePackages.kwin}/bin/kwin_wayland"
+    "--no-global-shortcuts"
+    "--no-kactivities"
+    "--no-lockscreen"
+    "--locale1"
+    "--inputmethod"
+    "maliit-keyboard"
+  ];
+  environment.systemPackages = [
+    pkgs.maliit-framework
+    pkgs.maliit-keyboard
+  ];
 }
