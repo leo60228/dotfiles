5286380c0750601e733f416ea057a32ac21545cb
leo60228 <leo@60228.dev>
2025-05-05 14:30:14 -0400

    leoasahi nfs

--- systems/leoasahi/default.nix
+++ systems/leoasahi/default.nix
@@ -4,4 +4,25 @@
   imports = [ ./hardware.nix ];
 
   system.stateVersion = "18.03";
+
+  networking.firewall.allowedTCPPorts = [
+    4000
+    4001
+    4002
+  ];
+  networking.firewall.allowedUDPPorts = [
+    4000
+    4001
+    4002
+  ];
+
+  services.nfs.server = {
+    enable = true;
+    exports = ''
+      /home 100.64.0.0/10(insecure,rw,crossmnt)
+    '';
+    statdPort = 4000;
+    lockdPort = 4001;
+    mountdPort = 4002;
+  };
 }
