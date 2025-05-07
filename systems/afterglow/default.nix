cfb59bfee471632670ecae8839344f228a199cb8
leo60228 <leo@60228.dev>
2025-05-06 20:25:28 -0400

    init afterglow

---
+++ systems/afterglow/default.nix
@@ -0,0 +1,31 @@
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+
+{
+  imports = [ ./hardware.nix ];
+
+  system.stateVersion = "25.05";
+
+  vris.graphical = true;
+
+  boot.supportedFilesystems = [ "nfs" ];
+
+  services.displayManager.sessionPackages = [ pkgs.kdePackages.plasma-mobile ];
+  services.displayManager.sddm.wayland.compositorCommand = lib.concatStringsSep " " [
+    "${lib.getBin pkgs.kdePackages.kwin}/bin/kwin_wayland"
+    "--no-global-shortcuts"
+    "--no-kactivities"
+    "--no-lockscreen"
+    "--locale1"
+    "--inputmethod"
+    "maliit-keyboard"
+  ];
+  environment.systemPackages = [
+    pkgs.maliit-framework
+    pkgs.maliit-keyboard
+  ];
+}
---
+++ systems/afterglow/hardware.nix
@@ -0,0 +1,27 @@
+{
+  config,
+  lib,
+  pkgs,
+  flakes,
+  ...
+}:
+{
+  imports = [
+    (import "${flakes.mobile-nixos}/lib/configuration.nix" { device = "lenovo-homestar"; })
+  ];
+
+  networking.firewall.checkReversePath = lib.mkForce false;
+
+  fileSystems."/" = {
+    device = "/dev/disk/by-uuid/c85a48fa-bd42-4e20-993b-5ed87e8548f3";
+    fsType = "ext4";
+  };
+
+  boot.growPartition = false;
+
+  boot.initrd.luks.devices."lvm".device = "/dev/disk/by-uuid/6e45bd62-0f61-471b-8e08-cf0545b4f606";
+
+  zramSwap.enable = true;
+
+  hardware.firmware = [ pkgs.chromeos-sc7180-unredistributable-firmware ];
+}
