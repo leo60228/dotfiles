aed6462ff7c686138d94ca0be7633fd25028ecfe
leo60228 <leo@60228.dev>
2025-07-08 15:11:15 +0000

    leoPkgs.avmvc12: 0-unstable-2025-05-27 -> 0-unstable-2025-06-18

--- pkgs/avmvc12/default.nix
+++ pkgs/avmvc12/default.nix
@@ -11,13 +11,13 @@
 
 stdenv.mkDerivation rec {
   name = "avmvc12-${version}-${linuxPackages.kernel.version}";
-  version = "0-unstable-2025-05-27";
+  version = "0-unstable-2025-06-18";
 
   src = fetchFromGitHub {
     owner = "GloriousEggroll";
     repo = "AVMATRIX-VC12-4K-CAPTURE";
-    rev = "baf4f22206e04cd9e155d8389f5a40dd0f0f0f8a";
-    hash = "sha256-8Ikb8PQtxzA3/IaW1CGezajeiTVuJNdlDeUMwCcjfwQ=";
+    rev = "3a8f34fe2d383070e471e6ec25701232611684fd";
+    hash = "sha256-++EvbOjILfKm4I0cnp14X9s6awk8v6i41MdiGWnTTgA=";
   };
 
   sourceRoot = "source/src";
