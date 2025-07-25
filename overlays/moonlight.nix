03714241323542b81ccb88beb061613777d49853
leo60228 <leo@60228.dev>
2025-07-25 13:40:01 -0400

    local moonlight overlay

--- overlays/moonlight.nix
+++ overlays/moonlight.nix
@@ -1,1 +1,9 @@
-{ moonlight, ... }: moonlight.overlays.default
+{ moonlight, ... }:
+self: super:
+
+{
+  discord-canary = super.discord-canary.override {
+    withMoonlight = true;
+    inherit (moonlight.packages.${self.system}) moonlight;
+  };
+}
