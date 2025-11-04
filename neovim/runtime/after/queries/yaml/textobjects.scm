d8faf3175fa22d1706afabcc355271325a1868e4
leo60228 <leo@60228.dev>
2025-11-03 20:17:07 -0500

    standard block queries

--- neovim/runtime/after/queries/yaml/textobjects.scm
+++ neovim/runtime/after/queries/yaml/textobjects.scm
@@ -2,4 +2,4 @@
 (string_scalar) @string.inner
 ((double_quote_scalar) @string.inner (#offset! @string.inner 0 1 0 -1))
 ((single_quote_scalar) @string.inner (#offset! @string.inner 0 1 0 -1))
-(document (block_node) @paragraph)
+(document (block_node) @block.inner) @block.outer
