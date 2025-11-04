856ffa891e87244a1c9e25d0fde6f103c55e01c5
leo60228 <leo@60228.dev>
2025-11-03 20:01:31 -0500

    more mappings

--- neovim/runtime/after/queries/yaml/textobjects.scm
+++ neovim/runtime/after/queries/yaml/textobjects.scm
@@ -2,3 +2,4 @@
 (string_scalar) @string.inner
 ((double_quote_scalar) @string.inner (#offset! @string.inner 0 1 0 -1))
 ((single_quote_scalar) @string.inner (#offset! @string.inner 0 1 0 -1))
+(document (block_node) @paragraph)
