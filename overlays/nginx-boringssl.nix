76fea1b365e8ed5b3e8168ef15523ff6fb8e3116
leo60228 <leo@60228.dev>
2025-12-05 18:13:57 -0500

    nginx with aws-lc

--- overlays/nginx-boringssl.nix
+++ overlays/nginx-boringssl.nix
@@ -1,14 +1,6 @@
 self: super:
 
-let
-  override =
-    nginx:
-    (nginx.override { openssl = self.boringssl; }).overrideAttrs (oldAttrs: {
-      NIX_LDFLAGS = [ "-lstdc++" ]; # i hate it here
-    });
-in
 {
-  nginx = override super.nginx;
-  nginxStable = override super.nginxStable;
-  nginxMainline = override super.nginxMainline;
+  nginx = self.nginxMainline;
+  nginxMainline = super.nginxMainline.override { openssl = self.aws-lc; };
 }
