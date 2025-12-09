0deab9a4311a316d05db14f102dd0be70322fafa
leo60228 <leo@60228.dev>
2025-12-09 11:15:05 -0500

    emuhawk icon

--- overlays/bizhawk.nix
+++ overlays/bizhawk.nix
@@ -7,5 +7,17 @@ self: super:
     system = self.stdenv.hostPlatform.system;
     dotnet-sdk_5 = null;
   };
-  emuhawk = self.bizhawk.emuhawk-latest-bin;
+  emuhawk =
+    (self.bizhawk.buildEmuHawkInstallableFor {
+      bizhawkAssemblies = self.bizhawk.bizhawkAssemblies-latest-bin;
+      desktopName = "EmuHawk";
+      desktopIcon = "emuhawk";
+    }).overrideAttrs
+      (oldAttrs: {
+        paths = oldAttrs.paths ++ [
+          (self.runCommand "emuhawk-icon" { } ''
+            install -Dm644 "${bizhawk}/src/BizHawk.Client.EmuHawk/images/corphawk.png" $out/share/pixmaps/emuhawk.png
+          '')
+        ];
+      });
 }
