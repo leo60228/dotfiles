2b1be4cfde4f1a310c8dc541081ee83f7f4e7862
leo60228 <leo@60228.dev>
2026-01-22 18:20:27 -0500

    init cardamom

---
+++ systems/cardamom/hardware.nix
@@ -0,0 +1,39 @@
+{
+  modulesPath,
+  pkgs,
+  ...
+}:
+
+{
+  imports = [
+    (modulesPath + "/installer/scan/not-detected.nix")
+  ];
+
+  boot.kernelPackages = pkgs.linuxPackages_rpi4;
+  boot.initrd.availableKernelModules = [ "nvme" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ ];
+  boot.extraModulePackages = [ ];
+
+  boot.loader.systemd-boot.enable = true;
+
+  fileSystems."/" = {
+    device = "rpool/root";
+    fsType = "zfs";
+  };
+
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-uuid/6119-565E";
+    fsType = "vfat";
+    options = [
+      "fmask=0077"
+      "dmask=0077"
+    ];
+  };
+
+  swapDevices = [
+    { device = "/dev/disk/by-uuid/312e8bb2-b73f-42bc-960c-e17f215108a8"; }
+  ];
+
+  deployment.tags = [ "servers" ];
+}
