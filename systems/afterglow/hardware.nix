b0e5bd878c149430262b19e8bcbacafcfd366161
leo60228 <leo@60228.dev>
2026-02-19 18:15:06 -0500

    use nixos kernel with drivers in initrd

--- systems/afterglow/hardware.nix
+++ systems/afterglow/hardware.nix
@@ -24,6 +24,55 @@
 
   hardware.firmware = [ pkgs.chromeos-sc7180-unredistributable-firmware ];
 
+  mobile.boot.stage-1.kernel.useNixOSKernel = true;
+  boot.kernelPackages = pkgs.linuxPackages_6_18;
+  boot.initrd.includeDefaultModules = true;
+  boot.initrd.availableKernelModules = [
+    "hid_google_hammer"
+
+    # A whole bunch of CrOS EC drivers
+    "extcon_usbc_cros_ec"
+    "i2c_cros_ec_tunnel"
+    "cros_ec_lid_angle"
+    "cros_ec_sensors_core"
+    "cros_ec_sensors"
+    "cros_ec_keyb"
+    "cros_ec_dev"
+    "cros_ec_chardev"
+    "cros_ec_i2c"
+    "cros_ec"
+    "cros_ec_rpmsg"
+    "cros_ec_sensorhub"
+    "cros_ec_spi"
+    "cros_ec_sysfs"
+    "cros_ec_typec"
+    "cros_ec_vbc"
+    "cros_usbpd_notify"
+    "cros_usbpd_charger"
+    "pwm_cros_ec"
+    "cros_ec_regulator"
+    "rtc_cros_ec"
+    "snd_soc_cros_ec_codec"
+
+    # from nixos-mobile
+    "sbs-battery"
+    "sbs-charger"
+    "sbs-manager"
+
+    "msm"
+    "coreboot_table"
+
+    "panel_boe_tv101wum_nl6"
+
+    "snd_soc_sc7180"
+    "dispcc_sc7180"
+    "camcc_sc7180"
+    "videocc_sc7180"
+    "snd_soc_lpass_sc7180"
+    "lpasscorecc_sc7180"
+    "gpucc_sc7180"
+  ];
+
   vris.firefox =
     let
       wrapFirefox = pkgs.wrapFirefox.override {
