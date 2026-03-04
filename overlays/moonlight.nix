eb31df5fc7b260480d1a18851f4a534971c7083e
leo60228 <leo@60228.dev>
2026-03-04 16:57:26 -0500

    fix discord scaling

--- overlays/moonlight.nix
+++ overlays/moonlight.nix
@@ -5,5 +5,6 @@ self: super:
   inherit (moonlight.packages.${self.stdenv.hostPlatform.system}) moonlight;
   discord-canary = super.discord-canary.override {
     withMoonlight = true;
+    commandLineArgs = "--force-device-scale-factor=1.0";
   };
 }
