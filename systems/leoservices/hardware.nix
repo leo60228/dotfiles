b0bfed1067315ed9081d6a259eec9a73d0c90bf8
leo60228 <leo@60228.dev>
2026-04-16 18:27:50 -0400

    migrate leoservices to new provider

--- systems/leoservices/hardware.nix
+++ systems/leoservices/hardware.nix
@@ -1,23 +1,69 @@
 {
-  pkgs,
-  lib,
   modulesPath,
   ...
 }:
 {
-  imports = [ "${modulesPath}/virtualisation/amazon-image.nix" ];
-  systemd.services.amazon-init.enable = lib.mkForce false;
+  imports = [ "${modulesPath}/profiles/qemu-guest.nix" ];
 
-  systemd.services.NetworkManager-wait-online.enable = false;
-
-  swapDevices = [
-    {
-      device = "/var/swapfile";
-      size = 4096;
-    }
+  boot.initrd.availableKernelModules = [
+    "ata_piix"
+    "uhci_hcd"
+    "virtio_pci"
+    "virtio_scsi"
+    "sd_mod"
+    "sr_mod"
   ];
 
-  boot.loader.grub.device = lib.mkForce "/dev/nvme0n1";
+  boot.supportedFilesystems = [ "zfs" ];
+
+  boot.loader.grub.device = "/dev/sda";
+
+  fileSystems."/" = {
+    device = "rpool/root";
+    fsType = "zfs";
+  };
+
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-uuid/3099-F3B1";
+    fsType = "vfat";
+    options = [
+      "fmask=0077"
+      "dmask=0077"
+    ];
+  };
+
+  networking = {
+    dhcpcd.enable = false;
+    nameservers = [
+      "45.90.28.142"
+      "45.90.30.142"
+      "2a07:a8c0::c4:a844"
+      "2a07:a8c1::c4:a844"
+    ];
+    defaultGateway = "86.48.28.1";
+    defaultGateway6 = {
+      address = "fe80::1";
+      interface = "ens18";
+    };
+    interfaces = {
+      ens18 = {
+        ipv4.addresses = [
+          {
+            address = "86.48.30.173";
+            prefixLength = 22;
+          }
+        ];
+        ipv6.addresses = [
+          {
+            address = "2605:a144:2323:7818::1";
+            prefixLength = 64;
+          }
+        ];
+      };
+    };
+  };
+
+  boot.initrd.secrets."/var/lib/root-key" = null;
 
   deployment.tags = [ "servers" ];
 }
