087b5d9e3763319764488b669782f44037ff906f
leo60228 <leo@60228.dev>
2026-04-17 15:39:02 +0000

    leoPkgs.beetcamp: 0.23.0 -> 0.24.3

--- pkgs/beetcamp/default.nix
+++ pkgs/beetcamp/default.nix
@@ -11,14 +11,14 @@
 
 buildPythonApplication rec {
   pname = "beets-beetcamp";
-  version = "0.23.0";
+  version = "0.24.3";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "snejus";
     repo = "beetcamp";
     tag = version;
-    hash = "sha256-8FEDpobEGZ0Lw1+JRoFIEe3AuiuX7dwsRab+P3hC3W0=";
+    hash = "sha256-kKFYuTJys4j67+cak2PDmn6z2vNzVitFXIZXy2bClY8=";
   };
 
   nativeBuildInputs = [
