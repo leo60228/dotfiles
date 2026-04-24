e33975aa83f92d022cc7efb22b01a355d9b57ff1
leo60228 <leo@60228.dev>
2026-04-24 12:32:56 -0400

    nextcloud33

--- systems/greenhouse/default.nix
+++ systems/greenhouse/default.nix
@@ -335,6 +335,7 @@
   services.nextcloud = {
     enable = true;
     hostName = "nextcloud.l3.pm";
+    package = pkgs.nextcloud33;
     config.adminpassFile = "/var/lib/nextcloud-admin-pass";
     config.dbtype = "sqlite";
     https = true;
