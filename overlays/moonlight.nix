1db5a12ebd5480c11f38018876a4423cbda48a94
leo60228 <leo@60228.dev>
2026-07-25 13:11:20 -0400

    override all discord

--- overlays/moonlight.nix
+++ overlays/moonlight.nix
@@ -1,10 +1,17 @@
 { moonlight, ... }:
 self: super:
 
+let
+  override =
+    discord:
+    discord.override {
+      withMoonlight = true;
+      commandLineArgs = "--force-device-scale-factor=1.0";
+    };
+in
 {
   inherit (moonlight.packages.${self.stdenv.hostPlatform.system}) moonlight;
-  discord-canary = super.discord-canary.override {
-    withMoonlight = true;
-    commandLineArgs = "--force-device-scale-factor=1.0";
-  };
+  discord = override super.discord;
+  discord-ptb = override super.discord-ptb;
+  discord-canary = override super.discord-canary;
 }
