65b84751877eea1d57b9ea6f5854c81773314bca
leo60228 <leo@60228.dev>
2026-08-23 17:48:00 -0400

    fix url

--- overlays/wine.nix
+++ overlays/wine.nix
@@ -4,11 +4,11 @@ self: super: {
       unstable = super'.unstable.overrideAttrs (oldAttrs: {
         patches = oldAttrs.patches or [ ] ++ [
           (self.fetchpatch {
-            url = "https://raw.githubusercontent.com/Arecsu/wine-affinity/refs/heads/main/0006-comdlg32-use-XDG-Desktop-Portal-for-native-file-dial.patch";
+            url = "https://raw.githubusercontent.com/Arecsu/wine-affinity/3bb23c70fda730fc9d176b37cd116b51f2b3c15b/0006-comdlg32-use-XDG-Desktop-Portal-for-native-file-dial.patch";
             hash = "sha256-TJHQjeS0puNulfKvlKn5EcpzEFn/onQGg061tczKsF8=";
           })
           (self.fetchpatch {
-            url = "https://raw.githubusercontent.com/Arecsu/wine-affinity/refs/heads/main/0007-comdlg32-fix-portal-dialog-compatibility-and-respons.patch";
+            url = "https://raw.githubusercontent.com/Arecsu/wine-affinity/3bb23c70fda730fc9d176b37cd116b51f2b3c15b/0007-comdlg32-fix-portal-dialog-compatibility-and-respons.patch";
             hash = "sha256-JEGZ5fPORBbOE8ATsAQ2T5McaOrccr6JAZJKOlYaKbw=";
           })
         ];
