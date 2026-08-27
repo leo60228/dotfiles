0ef57d6e81334061fac87a5fe3012bb4817d761b
leo60228 <leo@60228.dev>
2026-08-27 18:43:03 -0400

    remove pebble cache

--- nixos/cachix/pebble.nix
+++
@@ -1,6 +0,0 @@
-{
-  nix.settings = {
-    substituters = [ "https://pebble.cachix.org" ];
-    trusted-public-keys = [ "pebble.cachix.org-1:aTqwT2hR6lGggw/rPISRcHZctDv2iF7ewsVxf3Hq6ow=" ];
-  };
-}
