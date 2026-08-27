2afa06eef593fab45e2a3de4d6d40162e5874429
leo60228 <leo@60228.dev>
2026-08-27 19:36:01 -0400

    disable OIDC-Lite tests

--- systems/desktop/default.nix
+++ systems/desktop/default.nix
@@ -58,7 +58,25 @@
   # Hydra {{{1
   services.hydra-dev = {
     enable = true;
-    package = pkgs.hydra;
+    package = pkgs.hydra.overrideAttrs (oldAttrs: {
+      nativeBuildInputs = map (
+        x:
+        if x.name == "hydra-perl-deps" then
+          x.override {
+            paths = map (
+              x:
+              if x ? pname && x.pname == "OIDC-Lite" then
+                x.overrideAttrs (oldAttrs: {
+                  doCheck = false;
+                })
+              else
+                x
+            ) x.paths;
+          }
+        else
+          x
+      ) oldAttrs.nativeBuildInputs;
+    });
     hydraURL = "https://hydra.capybara-pirate.ts.net";
     port = 9999;
     notificationSender = "hydra@60228.dev";
